cdf1283829b8ccc1b1ec288a2744e201b025af0b
Alexander Eriksson <alex.store@hotmail.com>
2022-11-27 21:17:14 +0100

    styled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,14 +16,15 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-2xl font-extrabold tracking-tight text-white sm:text-[2rem]">
-            Enter an <span className="text-[hsl(280,100%,70%)]">EU West</span>{" "}
-            username
+            Search <span className="text-[hsl(280,100%,70%)]">EU West</span>{" "}
+            Summoner Profiles
           </h1>
           <div className="grid grid-rows-1 gap-4 sm:grid-rows-2 md:gap-8">
             <input
               type="text"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
+              className="w-full rounded bg-gray-700 text-center text-xl text-white placeholder-gray-400 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-gray-500"
             />
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
