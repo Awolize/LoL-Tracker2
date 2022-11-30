5c19a646cc2bc36b3161eda8ccd86eea9cab28cd
Alexander Eriksson <alex.store@hotmail.com>
2022-11-30 20:04:51 +0100

    remove prisma

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,8 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 // Prisma adapter for NextAuth, optional and can be removed
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-import { prisma } from "../../../server/db/client";
+// import { prisma } from "../../../server/db/client";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -15,7 +15,7 @@ export const authOptions: NextAuthOptions = {
     },
   },
   // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
+  //   adapter: PrismaAdapter(prisma),
   providers: [],
 };
 
