09be3829d1c20dfac9492793021f0a7c5d199e79
Alexander Eriksson <alex.store@hotmail.com>
2022-12-01 23:22:47 +0100

    fixed unnecessary trpc requests and ran prettier

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,74 @@
+import { Fragment, useEffect, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+
+interface Props {
+  callback: (data: number) => void;
+  choices: { text: string; value: number }[];
+  defaultIndex: number;
+}
+
+const Dropdown = ({ callback, choices, defaultIndex }: Props) => {
+  const [selected, setSelected] = useState(choices[defaultIndex]);
+
+  useEffect(() => {
+    if (selected?.value != null) {
+      callback(selected.value);
+    }
+  }, [callback, selected?.value]);
+
+  return (
+    <Listbox value={selected} onChange={setSelected}>
+      <div className="relative mt-1">
+        <Listbox.Button className="relative w-full cursor-default rounded-lg bg-gray-900 py-2 pl-3 pr-10 text-left text-sm text-gray-100 shadow-md">
+          <span className="block truncate">{selected?.text}</span>
+          <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+            <ChevronUpDownIcon
+              className="h-5 w-5 text-gray-100"
+              aria-hidden="true"
+            />
+          </span>
+        </Listbox.Button>
+        <Transition
+          as={Fragment}
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Listbox.Options className="max-h-75 absolute mt-1 w-full overflow-auto rounded-md bg-gray-900 py-1 text-base text-gray-100 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            {choices.map((person, personIdx) => (
+              <Listbox.Option
+                key={personIdx}
+                className={({ active }) =>
+                  `relative cursor-default select-none py-2 pl-10 pr-4 text-gray-100 ${
+                    active ? "bg-gray-800" : ""
+                  }`
+                }
+                value={person}
+              >
+                {({ selected }) => (
+                  <>
+                    <span
+                      className={`block truncate ${
+                        selected ? "font-medium" : "font-normal"
+                      }`}
+                    >
+                      {person.text}
+                    </span>
+                    {selected ? (
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-3  text-gray-100">
+                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    ) : null}
+                  </>
+                )}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </Transition>
+      </div>
+    </Listbox>
+  );
+};
+
+export default Dropdown;
--- src/pages/[server]/[username]/mastery.tsx
+++ src/pages/[server]/[username]/mastery.tsx
@@ -1,11 +1,317 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import type {
+  ChampionMasteryDTO,
+  ChampionsDataDragonDetails,
+} from "twisted/dist/models-dto";
+import championJson from "./champions.json";
+import rolesJson from "./roles.json";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { DATA_DRAGON_URL } from "../../../utils/constants";
+import { Switch } from "@headlessui/react";
+import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/20/solid";
+import "react-lazy-load-image-component/src/effects/opacity.css";
+import Dropdown from "../../../components/Dropdown";
+import { trpc } from "../../../utils/trpc";
 import { useRouter } from "next/router";
-import ChampsWrapper from "../../../components/champ_mastery/Champs";
 
-export default function ProfilePage() {
-  const router = useRouter();
-  const { username } = router.query;
-  console.log(router.query);
-
-  return <ChampsWrapper username={username as string} />;
+interface Roles {
+  role: string;
 }
+
+type CompleteChamptionInfo = ChampionMasteryDTO &
+  ChampionsDataDragonDetails &
+  Roles;
+type incompleteCompleteChamptionInfo = ChampionsDataDragonDetails & Roles;
+
+const ChampsWrapper = () => {
+  const router = useRouter();
+  const { server, username } = router.query;
+  const { data, isFetched } = trpc.riotApi.getUserByName.useQuery(
+    { username: username as string },
+    { enabled: typeof username == "string" }
+  );
+
+  if (isFetched && data?.id) {
+    return <Champs userId={data?.id} />;
+  } else {
+    return (
+      <main>Welcome {username} getting your user info information...</main>
+    );
+  }
+};
+
+export default ChampsWrapper;
+
+const Champs = ({ userId }: { userId: string }) => {
+  const [championMastery, setChampionMastery] =
+    useState<CompleteChamptionInfo[]>();
+
+  const { data, isLoading, isFetched } =
+    trpc.riotApi.getMasteryPointsById.useQuery(
+      { id: userId },
+      { enabled: userId != null }
+    );
+
+  useEffect(() => {
+    if (championMastery == null && data) {
+      const list = Object.keys(championJson.data).map((champName) => {
+        const jsonInfo =
+          championJson.data[champName as keyof typeof championJson.data];
+
+        const personalChampData = data
+          .filter((champ) => `${champ.championId}` == jsonInfo.key)
+          .at(0);
+
+        // build champ objects
+        const champ: CompleteChamptionInfo | incompleteCompleteChamptionInfo = {
+          ...jsonInfo,
+          ...personalChampData,
+          name: jsonInfo.name == "Nunu & Willump" ? "Nunu" : jsonInfo.name,
+          championPoints: personalChampData?.championPoints ?? 0,
+          championLevel: personalChampData?.championLevel ?? 0,
+          role:
+            rolesJson[jsonInfo.id as keyof typeof championJson.data] ??
+            "Unknown",
+        };
+        return champ;
+      });
+
+      setChampionMastery(list as CompleteChamptionInfo[]);
+    }
+  }, [championMastery, data]);
+
+  const listItem = (champ: CompleteChamptionInfo) => {
+    const disabled = filteredOut(champ);
+    const hideAll = disabled && !showFinished;
+
+    return (
+      <li className="flex flex-col pb-2" key={champ.key as React.Key}>
+        {/* Image doesnt work in production, only loads about 6 images and then times out on the rest, container restrictions (ram,etc)? */}
+
+        <div className="relative z-10">
+          {showLevels && !hideAll && (
+            <span className="absolute top-[3px] left-[3px] flex h-6 w-6 items-center justify-center bg-blue-800 px-[0.40rem] text-center text-xs leading-5">
+              {champ.championLevel}
+            </span>
+          )}
+          <LazyLoadImage
+            src={`${DATA_DRAGON_URL}${champ.image.full}`}
+            style={{
+              zIndex: -1,
+              opacity: disabled ? "40%" : "100%",
+            }}
+            className={` ${disabled ? "grayscale" : ""}`}
+            alt={`${champ.name}`}
+            height={90}
+            width={90}
+            effect="opacity"
+            hidden={hideAll}
+            // placeholderSrc="/placeholder.png"
+          />
+        </div>
+
+        <div className="text-center text-xs">{!hideAll && champ.name}</div>
+        <div className="items-center justify-center text-center text-xs">
+          {!hideAll && champ.championPoints}
+        </div>
+      </li>
+    );
+  };
+
+  const [filterPoints, setFilterPoints] = useState(0);
+  const [sortOrder, setSortOrder] = useState(0);
+
+  const filteredOut = (champ: CompleteChamptionInfo) => {
+    const disabled: boolean = champ.championPoints > filterPoints;
+    return disabled;
+  };
+  const sortAlgorithm = (
+    a: CompleteChamptionInfo,
+    b: CompleteChamptionInfo
+  ): number => {
+    switch (sortOrder) {
+      case 0:
+        return a.championPoints > b.championPoints ? -1 : 1;
+      case 1:
+        return a.name.localeCompare(b.name);
+      case 2:
+        if (a.championLevel === b.championLevel)
+          return a.championPoints > b.championPoints ? -1 : 1;
+        else return a.championLevel > b.championLevel ? -1 : 1;
+      default:
+        return a.name.localeCompare(b.name);
+    }
+  };
+
+  const markedSize: number =
+    championMastery?.filter((champ) => filteredOut(champ)).length ?? 0;
+
+  const [showLevels, setShowLevels] = useState(false);
+  const [showFinished, setShowFinished] = useState(false);
+  const [alignHeaderRight, setAlignHeaderRight] = useState(false);
+
+  if (!isLoading && isFetched && championMastery)
+    return (
+      <>
+        <header className="relative mt-2 flex justify-center">
+          <div
+            className={`absolute top-6 z-50 flex w-full flex-row items-center  justify-start  gap-4 px-4 ${
+              alignHeaderRight ? "flex-row-reverse" : "flex-row"
+            }`}
+          >
+            <div className="w-32">
+              <Dropdown
+                callback={setFilterPoints}
+                defaultIndex={4}
+                choices={[
+                  { text: "All", value: Number.MAX_SAFE_INTEGER },
+                  { text: "100", value: 100 },
+                  { text: "500", value: 500 },
+                  { text: "1,000", value: 1000 },
+                  { text: "5,000", value: 5000 },
+                  { text: "10,000", value: 10000 },
+                  { text: "21,600", value: 21600 },
+                  { text: "50,000", value: 50000 },
+                  { text: "100,000", value: 100000 },
+                ]}
+              />
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              Sort:
+              <div className="w-32">
+                <Dropdown
+                  callback={setSortOrder}
+                  defaultIndex={0}
+                  choices={[
+                    { text: "Points", value: 0 },
+                    { text: "A-Z", value: 1 },
+                    { text: "Level", value: 2 },
+                  ]}
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-row items-center gap-2">
+              <span>Show Levels</span>
+              <Switch
+                checked={showLevels}
+                onChange={setShowLevels}
+                className={`${
+                  showLevels
+                    ? "bg-blue-600"
+                    : "bg-gradient-to-r from-indigo-500 to-purple-500"
+                } relative inline-flex h-6 w-11 items-center rounded-full`}
+              >
+                <span className="sr-only">Show Levels</span>
+                <span
+                  className={`${
+                    showLevels ? "translate-x-6" : "translate-x-1"
+                  } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+                />
+              </Switch>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <span>Show finished</span>
+              <Switch
+                checked={showFinished}
+                onChange={setShowFinished}
+                className={`${
+                  showFinished
+                    ? "bg-blue-600"
+                    : "bg-gradient-to-r from-indigo-500 to-purple-500"
+                } relative inline-flex h-6 w-11 items-center rounded-full`}
+              >
+                <span className="sr-only">Show finished</span>
+                <span
+                  className={`${
+                    showFinished ? "translate-x-6" : "translate-x-1"
+                  } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+                />
+              </Switch>
+            </div>
+
+            <div className="flex pr-4">
+              <button onClick={() => setAlignHeaderRight((prev) => !prev)}>
+                <span className="absolute inset-y-0 flex items-center pr-2">
+                  {alignHeaderRight ? (
+                    <ChevronLeftIcon
+                      className="h-5 w-5 text-gray-100"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    <ChevronRightIcon
+                      className="h-5 w-5 text-gray-100"
+                      aria-hidden="true"
+                    />
+                  )}
+                </span>
+              </button>
+            </div>
+          </div>
+
+          <div className="rounded-xl bg-gradient-to-r from-green-500 via-sky-500 to-purple-500 p-[3px]">
+            <div className="flex h-full flex-col  justify-between rounded-lg bg-black px-4 py-2 text-center text-white ">
+              <p className="text-2xl">
+                {markedSize} / {championMastery?.length}
+              </p>
+              <p className="text-sm">
+                {((100 * markedSize) / championMastery?.length).toFixed(2)}%{" "}
+              </p>
+            </div>
+          </div>
+        </header>
+
+        <main>
+          <div className="flex flex-row gap-2">
+            {["Top", "Jungle", "Mid", "Bottom", "Support"].map((role) => {
+              const champsWithRole = championMastery.filter((champ) => {
+                return champ.role === role;
+              });
+
+              const doneChamps = champsWithRole
+                .filter(filteredOut)
+                .sort(sortAlgorithm);
+
+              const todoChamps = champsWithRole
+                .filter((champ) => !filteredOut(champ))
+                .sort(sortAlgorithm);
+
+              const size: number = champsWithRole.length;
+              const markedSize: number = champsWithRole.filter((champ) =>
+                filteredOut(champ)
+              ).length;
+              const percentage = (100 * markedSize) / size;
+
+              return (
+                <div className="w-full p-4" key={role}>
+                  <div className="text-md flex flex-row justify-center gap-8 align-bottom ">
+                    <h4 className="my-auto p-2  ">
+                      {markedSize} / {size}
+                    </h4>
+                    <div className="mb-2 bg-gradient-to-r from-green-600 via-sky-600 to-purple-600 pb-[3px]">
+                      <div className="flex h-full flex-col justify-between bg-black text-gray-200 ">
+                        <h4 className="text-xl font-bold">{role}</h4>
+                      </div>
+                    </div>
+                    <h4 className="my-auto p-2 ">{percentage.toFixed(1)}%</h4>
+                  </div>
+                  <ul
+                    className="grid justify-between"
+                    style={{
+                      gridTemplateColumns: "repeat(auto-fill, 90px)",
+                    }}
+                  >
+                    {todoChamps.map((champ) => listItem(champ))}
+                    {doneChamps.map((champ) => listItem(champ))}
+                  </ul>
+                </div>
+              );
+            })}
+          </div>
+        </main>
+      </>
+    );
+  else {
+    return <main>Getting your user info information...</main>;
+  }
+};
