16173ecc84597c2c071d9fbe4417791459f711c3
Alexander Eriksson <alex.store@hotmail.com>
2023-01-04 23:40:20 +0100

    update build errors

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,15 +1,17 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
 import { env } from "../../../env/server.mjs";
-import { appRouter } from "../../../server/trpc/router/_app";
+import { createTRPCContext } from "../../../server/api/trpc";
+import { appRouter } from "../../../server/api/root";
 
 // export API handler
 export default createNextApiHandler({
-  router: appRouter,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(`❌ tRPC failed on ${path}: ${error}`);
-        }
-      : undefined,
+    router: appRouter,
+    createContext: createTRPCContext,
+    onError:
+        env.NODE_ENV === "development"
+            ? ({ path, error }) => {
+                console.error(`❌ tRPC failed on ${path}: ${error}`);
+            }
+            : undefined,
 });
