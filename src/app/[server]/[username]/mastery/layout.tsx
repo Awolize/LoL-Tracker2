5e1ed6b409498cc226d4e655db655dac0da8c5ab
Alexander Eriksson <alex.store@hotmail.com>
2023-12-18 21:13:19 +0100

    removed unused layout, test if SSO gets worse

--- src/app/[server]/[username]/mastery/layout.tsx
+++ src/app/[server]/[username]/mastery/layout.tsx
@@ -1,16 +1,8 @@
 export async function generateMetadata({ params: { server, username } }) {
     return {
-        title: "LoL Mastery Tracker, brought to you by me!",
+        title: `LoL Mastery Tracker. ${username} Profile`,
         description:
             "Made using Riot API. Repo can be found using https://github.com/Awolize. Boilerplate was generated using https://create.t3.gg/",
         keywords: [server, username, "LoL", "mastery", "tracker"],
     };
-}
-
-export default function Layout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return children;
 }
