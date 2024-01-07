6da569d36b67224e7d92eccc23c9a79df17bb046
Alexander Eriksson <alex.store@hotmail.com>
2024-01-07 22:39:28 +0100

    table style fixup

--- src/app/[server]/[username]/mastery/components/match-history-item.tsx
+++ src/app/[server]/[username]/mastery/components/match-history-item.tsx
@@ -1,12 +1,8 @@
-import Image from "next/image";
-import { useDataDragonPath } from "~/app/_components/use-data-dragon-path";
 import { useUserContext } from "../stores/user-store";
+import MatchTable from "./match-history-item-table";
 import { type CompleteMatch } from "./server-processing-helpers";
 
 const MatchItem = ({ match }: { match: CompleteMatch }) => {
-    // console.log(match.MatchInfo);
-
-    const { getChampionImage } = useDataDragonPath();
     const user = useUserContext((s) => s.user);
 
     // riotIdGameName
@@ -22,143 +18,16 @@ const MatchItem = ({ match }: { match: CompleteMatch }) => {
 
     return (
         <div className="w-full p-4 flex flex-row gap-2">
+            <div className="flex flex-row gap-4">
+                <MatchTable players={match.MatchInfo.participants as unknown as Array<any>} teamId={200} />
+                <MatchTable players={match.MatchInfo.participants as unknown as Array<any>} teamId={100} />
+            </div>
+
             <div className="flex flex-col">
                 <div>{match.MatchInfo.gameStartTimestamp.toLocaleString()}</div>
                 <div>{match.MatchInfo.gameMode}</div>
                 <div>{isWin ? "Win ✅" : "Lose ❌"}</div>
                 <div>{(match.MatchInfo.gameDuration / 60).toPrecision(3)} min</div>
-            </div>
-            <div className="flex flex-row gap-4">
-                <div className="relative overflow-x-auto">
-                    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" className="px-2 py-2">
-                                    Champion
-                                </th>
-                                <th scope="col" className="px-2 py-2">
-                                    Player
-                                </th>
-                                <th scope="col" className="px-2 py-2">
-                                    K/D/A
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {(match.MatchInfo.participants as unknown as Array<any>)
-                                ?.filter((a) => a.teamId === 200)
-                                .map((player) => (
-                                    <tr
-                                        className={
-                                            user.puuid === player.puuid
-                                                ? "bg-gray-50 border-b dark:bg-gray-700 dark:border-gray-700"
-                                                : "bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-                                        }
-                                        key={player.puuid}
-                                    >
-                                        <th
-                                            scope="row"
-                                            className="px-2 py-2 gap-1 flex flex-col justify-center font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                                        >
-                                            <div className="flex justify-center">
-                                                <Image
-                                                    src={getChampionImage(`${player.championName}.png`)}
-                                                    className="rounded"
-                                                    alt={`${player.championName}`}
-                                                    height={40}
-                                                    width={40}
-                                                    // fill
-                                                    unoptimized={true}
-                                                    // hidden={hideAll}
-                                                    placeholder="blur"
-                                                    blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
-                                                />
-                                            </div>
-                                            <div className="flex justify-center">{player.championName}</div>
-                                        </th>
-                                        <th
-                                            scope="row"
-                                            className={
-                                                "px-2 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                                            }
-                                        >
-                                            {player.riotIdGameName}
-                                        </th>
-                                        <th
-                                            scope="row"
-                                            className="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                                        >
-                                            {player.kills}/{player.deaths}/{player.assists}
-                                        </th>
-                                    </tr>
-                                ))}
-                        </tbody>
-                    </table>
-                </div>
-                <div className="relative overflow-x-auto">
-                    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" className="px-2 py-2">
-                                    Champion
-                                </th>
-                                <th scope="col" className="px-2 py-2">
-                                    Player
-                                </th>
-                                <th scope="col" className="px-2 py-2">
-                                    K/D/A
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {(match.MatchInfo.participants as unknown as Array<any>)
-                                ?.filter((a) => a.teamId === 100)
-                                .map((player) => (
-                                    <tr
-                                        className={
-                                            user.puuid === player.puuid
-                                                ? "bg-gray-50 border-b dark:bg-gray-700 dark:border-gray-700"
-                                                : "bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-                                        }
-                                        key={player.puuid}
-                                    >
-                                        <th
-                                            scope="row"
-                                            className="px-2 py-2 gap-1 flex flex-col justify-center font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                                        >
-                                            <div className="flex justify-center">
-                                                <Image
-                                                    src={getChampionImage(`${player.championName}.png`)}
-                                                    className="rounded"
-                                                    alt={`${player.championName}`}
-                                                    height={40}
-                                                    width={40}
-                                                    // fill
-                                                    unoptimized={true}
-                                                    // hidden={hideAll}
-                                                    placeholder="blur"
-                                                    blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
-                                                />
-                                            </div>
-                                            <div className="flex justify-center">{player.championName}</div>
-                                        </th>
-                                        <th
-                                            scope="row"
-                                            className="px-2 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                                        >
-                                            {player.riotIdGameName}
-                                        </th>
-                                        <th
-                                            scope="row"
-                                            className="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                                        >
-                                            {player.kills}/{player.deaths}/{player.assists}
-                                        </th>
-                                    </tr>
-                                ))}
-                        </tbody>
-                    </table>
-                </div>
             </div>
 
             {/* <div>{JSON.stringify(match.MatchInfo)}</div> */}
--- src/app/[server]/[username]/mastery/components/side-bar-expandable.tsx
+++ src/app/[server]/[username]/mastery/components/side-bar-expandable.tsx
@@ -5,16 +5,16 @@ function SideBarExpandable({ children, alignment }: { children: React.ReactNode;
 
     return (
         <div
-            className={`fixed bg-slate-900 h-screen z-20 duration-300 rounded-r-lg 
-                        ${drawerOpen ? "px-10 min-w-fit w-1/3" : "px-0 w-0"}
-                        ${alignment === "right" ? "right-0" : "left-0"}
-                        `}
+            className={`fixed bg-zinc-950 h-screen z-20 duration-300 rounded-r-lg 
+                ${drawerOpen ? "px-10 min-w-fit w-1/3" : "px-0 w-0"}
+                ${alignment === "right" ? "right-0" : "left-0"}
+            `}
         >
             <button
                 type="button"
                 onClick={() => setDrawerOpen(!drawerOpen)}
-                className={`bg-slate-900 whitespace-nowrap text-white rounded border-black cursor-pointer mt-5 " ${
-                    drawerOpen ? "flex flex-1 mx-auto p-2 z-30" : "relative flex flex-1 right-44  p-2 z-30"
+                className={`bg-zinc-900 whitespace-nowrap text-white rounded border-black cursor-pointer mt-5 py-2 px-4  " ${
+                    drawerOpen ? "flex flex-1 mx-auto z-30" : "relative flex flex-1 right-44 z-30"
                 }`}
             >
                 Match History
