ba33e07545146c2aee668135f7b2123e629cd39a
Alexander Eriksson <alex.store@hotmail.com>
2024-01-14 11:19:47 +0100

    test

--- src/app/[server]/[username]/mastery/components/side-bar-expandable.tsx
+++ src/app/[server]/[username]/mastery/components/side-bar-expandable.tsx
@@ -1,23 +1,24 @@
 import { useState } from "react";
 
 export function SideBarExpandable({ children, alignment }: { children: React.ReactNode; alignment: "right" | "left" }) {
-    const [drawerOpen, setDrawerOpen] = useState(false);
+    const [drawerOpen, setDrawerOpen] = useState(true);
 
     return (
         <div
             className={`fixed flex flex-col bg-zinc-950 py-2 z-20 duration-300 w-[1100px]
-                ${drawerOpen ? `${alignment}-0` : `${alignment === "left" ? "-left" : "-right"}-[1100px]`}
+                ${drawerOpen ? `${alignment}-0` : `-${alignment}-[1100px]`}
             `}
         >
             <div className="relative flex flex-1">
                 <button
                     type="button"
                     onClick={() => setDrawerOpen(!drawerOpen)}
-                    className={`bg-zinc-900 whitespace-nowrap text-white rounded border-black cursor-pointer mt-5 py-2 px-4 z-30 ${
-                        drawerOpen
-                            ? `absolute ${alignment}-1/2`
-                            : `absolute flex flex-1 w-fit ${alignment === "right" ? "-left" : "-right"}-44 z-30`
-                    }`}
+                    className={
+                        "bg-zinc-900 whitespace-nowrap text-white rounded border-black cursor-pointer mt-5 py-2 px-4 z-30 " +
+                        (drawerOpen
+                            ? `absolute left-1/2`
+                            : `absolute flex flex-1 w-fit ${alignment === "right" ? "-left" : "-right"}-44 z-30`)
+                    }
                 >
                     Match History
                 </button>
