7f2c46f5e9bae4ab0a86883baec9677306b3d810
Alexander Eriksson <alex.store@hotmail.com>
2024-01-31 20:10:40 +0100

    update sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -7,11 +7,11 @@ export async function generateSitemaps() {
     const prisma = new PrismaClient();
     const summonersSize = await prisma.summoner.count();
     const numberOfSitemaps = Math.ceil(summonersSize / SUMMONERS_PER_SITEMAP);
-    const resultArray = Array.from({ length: numberOfSitemaps }, (_, index) => ({ sitemapId: index }));
+    const resultArray = Array.from({ length: numberOfSitemaps }, (_, index) => ({ id: index }));
     return resultArray;
 }
 
-export default async function sitemap({ sitemapId }) {
+export default async function sitemap({ id: sitemapId }) {
     const summoners = (await getSummoners(sitemapId)).filter((e) => e.gameName && e.tagLine);
     const summonersSitemaps = summoners.map(({ gameName, tagLine, updatedAt }) => ({
         url: `${BASE_URL}/EUW/${gameName}-${tagLine}`,
