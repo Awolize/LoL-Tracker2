b9bfd2fbb227dab4368ca55f248cc310c1db2821
Awolize <alex.store@hotmail.com>
2024-06-21 01:09:08 +0200

    ignore eslint when building

--- next.config.mjs
+++ next.config.mjs
@@ -46,6 +46,11 @@ const config = {
 			},
 		];
 	},
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	  },
 };
 
 // // eslint-disable-next-line @typescript-eslint/no-var-requires
