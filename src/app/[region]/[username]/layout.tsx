3cedb9a7d93ac34ad1d49b48604d936cb36fbba5
Awolize <alex.store@hotmail.com>
2024-06-24 00:32:32 +0200

    tailwind biome sort classes

--- src/app/[region]/[username]/layout.tsx
+++ src/app/[region]/[username]/layout.tsx
@@ -18,13 +18,13 @@ export default function RootLayout({
 }) {
 	return (
 		<div className="flex flex-col">
-			<header className="flex flex-col sticky w-screen items-end border-b-4 bg-background z-30 top-0">
+			<header className="sticky top-0 z-30 flex w-screen flex-col items-end border-b-4 bg-background">
 				<SearchBar />
 			</header>
 
 			{children}
 
-			<footer className="flex flex-col items-center text-sm opacity-50 p-2">
+			<footer className="flex flex-col items-center p-2 text-sm opacity-50">
 				'lol.awot.dev' isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or
 				anyone officially involved in producing or managing Riot Games properties. Riot Games, and all
 				associated properties are trademarks or registered trademarks of Riot Games, Inc.
