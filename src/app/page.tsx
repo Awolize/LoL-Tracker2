40a88bd575c7299e044bbe067bda8159708b9fcc
Awolize <alex.store@hotmail.com>
2024-06-25 16:00:30 +0200

    new landing page structure design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useRef, useState } from "react";
+import { ArrowRightEndOnRectangleIcon as SubmitIcon } from "@heroicons/react/24/outline";
 
 import LoadingComponent from "../components/old/loading-spinner";
 import RegionListSelector, { regions } from "../components/old/region-list-selector";
@@ -38,37 +39,48 @@ export default function Page() {
 			<main
 				className={`flex min-h-screen flex-col items-center justify-center bg-[url('/background-1.webp')] bg-center bg-cover`}
 			>
-				<div className="flex w-full animate-pulse2 flex-col items-center justify-center gap-12 bg-black py-16">
-					<h1 className="font-extrabold text-2xl text-foreground tracking-tight sm:text-[2rem] ">
-						Search{" "}
-						<div className="inline-block">
-							<RegionListSelector selectedRegion={selectedRegion} setSelectedRegion={setSelectedRegion} />
-						</div>{" "}
-						Summoner Profile
-					</h1>
-					<form className="grid grid-rows-1 gap-4 sm:grid-rows-2 md:gap-8" onSubmit={onSubmit}>
-						<div className={"flex flex-col gap-1"}>
-							<input
-								type="text"
-								ref={usernameRef}
-								placeholder="lol.awot#dev"
-								className="h-12 w-full rounded bg-gray-700 text-center text-xl placeholder-gray-400 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-gray-500"
-							/>
-							<div className="text-foreground/50 text-xs">
-								Remember to include the # and tagline like: Awot#dev
-							</div>
+				{/* <div className="flex h-full w-full flex-col items-center justify-center gap-4 px-4 py-2"> */}
+				<div className="flex w-full animate-pulse2 flex-col items-center justify-center gap-4 bg-black py-16">
+					<div>
+						<h1 className="text-center font-extrabold text-2xl text-foreground tracking-tight sm:text-[2rem] ">
+							Awot's Challenges Tracker
+						</h1>
+						<h6 className="text-center text-foreground text-xl tracking-tight">League of Legends</h6>
+					</div>
+					<div>
+						<div className="flex h-full w-full flex-row items-center justify-center gap-4 px-4 py-2">
+							<h1 className="font-extrabold text-2xl text-foreground tracking-tight sm:text-[2rem] ">
+								<div className="inline-block">
+									<RegionListSelector
+										selectedRegion={selectedRegion}
+										setSelectedRegion={setSelectedRegion}
+									/>
+								</div>
+							</h1>
+							<form id="search-summoner" className="flex flex-row gap-[0.125rem]" onSubmit={onSubmit}>
+								<div>
+									<div className="flex flex-col gap-1">
+										<input
+											type="text"
+											ref={usernameRef}
+											placeholder="lol.awot#dev"
+											className="h-12 w-full rounded-l bg-gray-700 text-center text-xl placeholder-gray-400 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-gray-500"
+										/>
+										<div className="text-foreground/50 text-xs">
+											Remember to include the # and tagline like: Awot#dev
+										</div>
+									</div>
+								</div>
+								<button
+									form="search-summoner"
+									type="submit"
+									className="h-12 w-8 rounded-r bg-gray-700 p-1 align-middle hover:bg-gray-600"
+								>
+									{loading ? <LoadingComponent /> : <SubmitIcon />}
+								</button>
+							</form>
 						</div>
-						<button
-							type="submit"
-							className="flex items-center justify-center gap-4 rounded-xl bg-white/10 p-4 text-foreground hover:bg-white/20"
-						>
-							{loading ? (
-								<LoadingComponent />
-							) : (
-								<h3 className="justify-center text-center font-bold">Look up</h3>
-							)}
-						</button>
-					</form>
+					</div>
 				</div>
 			</main>
 		</>
