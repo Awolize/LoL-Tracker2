6432883fb69ee6eac503f099d40a03d6b8928a6a
Awolize <alex.store@hotmail.com>
2024-06-25 20:51:53 +0200

    optional params to maintext

--- src/components/header/MainText.tsx
+++ src/components/header/MainText.tsx
@@ -9,7 +9,7 @@ type TailwindFontWeight =
 	| "extrabold"
 	| "black";
 
-export const MainText = ({ bold = "extrabold", lg = true }: { bold: TailwindFontWeight; lg: boolean }) => {
+export const MainText = ({ bold = "extrabold", lg = true }: { bold?: TailwindFontWeight; lg?: boolean }) => {
 	return (
 		<h1
 			className={`text-center ${`font-${bold}`} ${lg ? "text-[2rem]" : "text-[1.5rem]"} text-foreground tracking-tight `}
