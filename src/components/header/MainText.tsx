ac51267c4f6f3320b609ff829cf158c4ea488ac4
Awolize <alex.store@hotmail.com>
2024-07-23 20:58:33 +0200

    Awot's Challenge Tracker

--- src/components/header/MainText.tsx
+++ src/components/header/MainText.tsx
@@ -14,7 +14,7 @@ export const MainText = ({ bold = "extrabold", lg = true }: { bold?: TailwindFon
 		<h1
 			className={`text-center ${`font-${bold}`} ${lg ? "text-[2rem]" : "text-[1.5rem]"} text-foreground tracking-tight `}
 		>
-			Awot's Challenges Tracker
+			Awot's Challenge Tracker
 		</h1>
 	);
 };
