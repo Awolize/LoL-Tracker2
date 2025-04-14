43d4a33a507da0d08fdac27308d2495ea74a85e4
Awolize <alex.store@hotmail.com>
2025-04-14 22:17:49 +0200

    Update dependencies, (React 19, TRPC 11)

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
+import { httpBatchStreamLink, loggerLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import { useState } from "react";
 
@@ -17,14 +17,14 @@ export function TRPCReactProvider(props: {
 
 	const [trpcClient] = useState(() =>
 		api.createClient({
-			transformer,
 			links: [
 				loggerLink({
 					enabled: (op) =>
 						process.env.NODE_ENV === "development" ||
 						(op.direction === "down" && op.result instanceof Error),
 				}),
-				unstable_httpBatchStreamLink({
+				httpBatchStreamLink({
+					transformer,
 					url: getUrl(),
 					headers() {
 						return {
